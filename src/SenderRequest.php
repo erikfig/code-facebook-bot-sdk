f3d070d2148a5369cb5372f3431897d0155d0bca
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-06-05 08:09:29 -0300

    classe que le a entrada de dados

---
+++ src/SenderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BotCode;
+
+class SenderRequest
+{
+    private $event;
+
+    public function __construct()
+    {
+        $event = file_get_contents("php://input");
+        $event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
+        $this->event = $event['entry'][0]['messaging'][0];
+    }
+
+    public function getSenderId()
+    {
+        return $this->event['sender']['id'] ?? null;
+    }
+
+    public function getMessage()
+    {
+        return $this->event['message']['text'] ?? null;
+    }
+
+    public function getPostback()
+    {
+        if (empty($this->event['postback'])) {
+            return null;
+        }
+        if (is_array($this->event['postback']) and !empty($this->event['postback']['payload'])) {
+            $this->event['postback']['payload'];
+        }
+        return $this->event['postback'];
+    }
+}
